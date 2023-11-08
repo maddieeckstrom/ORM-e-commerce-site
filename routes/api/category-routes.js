c1c50f9d0a0ce6f4598c2dab3dacbcde8d524839
Maddie <maddieeckstrom@gmail.com>
2023-11-07 16:38:16 -1000

    added all necessary columns and information to models folderR

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,34 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// find all categories by its id value
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [{model:Product}]
+  }).then((categoryData) => {
+    res.json(categoryData);
+  })
 });
 
+// find one category by its id value
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    include: [{model:Product}]
+  }).then((categoryData) => {
+    res.json(categoryData);
+  })
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create({
+      reader_id: req.body.reader_id,
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
